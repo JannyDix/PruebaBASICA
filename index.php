fbb6879e771173608448c677dfbe72d02b016ebf
Janeth Dix <alejandra.mdix@gmail.com>
2023-08-01 23:51:49 -0600

    Se cambia extencion de archivo a .php y se agregaron las librerias correspontientes

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Prueba</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    </head>
+    <body>
+        <div id="datos">
+            <button class ="btn-btn" type="button" onclick ="VerConsulta();"> Buscar</button>
+        </div>
+        <script src="./fun_prueba.js?<?php echo time(); ?>"></script>
+    </body>
+</html>
